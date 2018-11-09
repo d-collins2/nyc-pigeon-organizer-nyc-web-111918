d1606d7b6e600509385f63cb4a88ee4e88e3fcca
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-09 07:41:30 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,7 @@ def nyc_pigeon_organizer(data)
   data.each do |data_type, data_name|
     data_name.each do |selection, name|
       name.each do |el|
+        unless list.include
         list[el] = Hash.new 
         list[el][data_type] = []
         binding.pry
