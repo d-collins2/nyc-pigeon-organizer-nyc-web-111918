c1607682d8bbd96000e4408ce80fb89a544be9ef
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-09 07:21:09 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,10 @@ require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
   list = Hash.new
-  data.each do |data_type, selection|
-    binding.pry
+  data.each do |data_type, data_name|
+    data_name.each do |selection, name|
+      binding.pry
+    end 
   end 
   list 
 end
