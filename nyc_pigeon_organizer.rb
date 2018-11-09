f1ca40a7f7ee9d7b52617e0de0d7ce7240c1b9e3
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-09 07:26:05 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   data.each do |data_type, data_name|
     data_name.each do |selection, name|
       name.each do |el|
-        list[el] = data_type
+        list[el] = {}
         el[data_type] = []
         binding.pry
       end 
